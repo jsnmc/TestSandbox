e47e2dcb7935a965a29a3cb875a01dea0e09c8c3
jsnmc <jsnmc0@gmail.com>
2012-01-16 00:51:14 -0500

    ServiceStack test app using stand alone mode

---
+++ ServiceStackTest/Apphost.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+using Funq;
+using ServiceStack.Configuration;
+using ServiceStack.Logging;
+using ServiceStack.Logging.Support.Logging;
+using ServiceStack.WebHost.Endpoints;
+
+namespace ServiceStackTest
+{
+    /// <summary>
+    /// Configure ServiceStack to run in stand alone mode
+    /// </summary>
+    public class AppHost
+        : AppHostHttpListenerBase
+    {
+
+        private static string _ListenBaseUrl ="http://localhost:8080/";
+
+        public AppHost()
+            : base("Hello Web Services", typeof(HelloService).Assembly){}
+
+        public override void Configure(Container container)
+        {
+
+            Routes.Add<Hello>("/hello").Add<Hello>("/hello/{Name}");
+            try
+            {
+                this.Start(_ListenBaseUrl);
+            }
+            catch (System.Exception ex) 
+            {
+                Console.WriteLine("EXCEPTION: {0}", ex.Message);
+            }
+
+        }
+
+    }
+}
---
+++ ServiceStackTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ServiceStackTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var appHost = new AppHost();
+            appHost.Init();
+
+            Thread.Sleep(Timeout.Infinite);
+            System.Console.WriteLine("ReadLine()");
+            System.Console.ReadLine();
+        }
+    }
+
+    public class Hello
+    {
+        public string Name { get; set; }
+    }
+
+    public class HelloResponse
+    {
+        public string Result { get; set; }
+    }
+
+    public class HelloService : ServiceStack.ServiceHost.IService<Hello>
+    {
+        public object Execute(Hello request)
+        {
+            return new HelloResponse { Result = "Hello, " + request.Name };
+        }
+    }
+}
